67ffba8402ca4e9c8bbc2901153707e869f6c2b9
Mikhail Cherdakov <mishacherdakov@yandex.ru>
2022-01-21 21:42:55 +0000

    static files

--- hw6/hasker/hasker/urls.py
+++ hw6/hasker/hasker/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('users/', include('users.urls'), name='users'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
